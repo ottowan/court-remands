c0b18c4de6400adb68d68ceae057737476700aa8
parinya.coj <parinya.coj@gmail.com>
2019-10-30 21:26:24 +0700

    [Create] make new test for select courtname

--- courttype_test.go
+++ courttype_test.go
@@ -1,1 +1,43 @@
 package main
+
+import "testing"
+
+func Test_CheckCourtType_Input_1KweangCourt_ShouldBe_True(t *testing.T) {
+	input := 1
+	expected := true
+	actual := CheckCourtType(input)
+
+	if expected != actual {
+		t.Errorf("expected %s but it got %s", expected, actual)
+	}
+}
+
+func Test_CheckCourtType_Input_2ProvincialCourt_ShouldBe_True(t *testing.T) {
+	input := 2
+	expected := true
+	actual := CheckCourtType(input)
+
+	if expected != actual {
+		t.Errorf("expected %s but it got %s", expected, actual)
+	}
+}
+
+func Test_CheckCourtType_Input_2ProvincialCourt_ShouldBe_False(t *testing.T) {
+	input := 0
+	expected := false
+	actual := CheckCourtType(input)
+
+	if expected != actual {
+		t.Errorf("expected %s but it got %s", expected, actual)
+	}
+}
+
+func Test_CheckCourtType_Input_3ProvincialCourt_ShouldBe_False(t *testing.T) {
+	input := 3
+	expected := false
+	actual := CheckCourtType(input)
+
+	if expected != actual {
+		t.Errorf("expected %s but it got %s", expected, actual)
+	}
+}
