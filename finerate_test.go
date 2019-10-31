bdbee4349795c14f8a98391ec1a949ecaf8243c6
parinya.coj <parinya.coj@gmail.com>
2019-10-31 23:45:23 +0700

    [Create] make new testcase for test FineRateKwaeng

--- finerate_test.go
+++ finerate_test.go
@@ -1,1 +1,31 @@
 package main
+
+import "testing"
+
+func Test_CheckFineRateKwaeng_Input_fine_minus1_ShouldBe_True(t *testing.T) {
+	input := -1
+	expected := false
+	actual := CheckFineRateKwaeng(input)
+
+	if expected != actual {
+		t.Errorf("T7 : expected %s but it got %s", expected, actual)
+	}
+}
+func Test_CheckFineRateKwaeng_Input_fine_30000_ShouldBe_True(t *testing.T) {
+	input := 30000
+	expected := false
+	actual := CheckFineRateKwaeng(input)
+
+	if expected != actual {
+		t.Errorf("T8 : expected %s but it got %s", expected, actual)
+	}
+}
+func Test_CheckFineRateKwaeng_Input_fine_60001_ShouldBe_True(t *testing.T) {
+	input := 60001
+	expected := true
+	actual := CheckFineRateKwaeng(input)
+
+	if expected != actual {
+		t.Errorf("T9 : expected %s but it got %s", expected, actual)
+	}
+}
