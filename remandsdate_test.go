a0bdbc48ad9c39306cbb6ca125accf3115d6fd8a
thecob <14883363+thecob@users.noreply.github.com>
2019-10-31 22:07:10 +0700

    Add files via upload

--- remandsdate_test.go
+++ remandsdate_test.go
@@ -1,1 +1,42 @@
 package main
+import "testing"
+
+func Test_CheckRemandsDateProvincial_Input_0imprisonDate_ShouldBe_True(t *testing.T) {
+	input := 0
+	expected := false
+	actual := imprison_date(input)
+
+	if expected != actual {
+		t.Errorf("expected %s but it got %s", expected, actual)
+	}
+}
+
+func Test_CheckRemandsDateProvincial_Input_1imprisonDate_ShouldBe_True(t *testing.T) {
+	input := 1
+	expected := true
+	actual := imprison_date(input)
+
+	if expected != actual {
+		t.Errorf("expected %s but it got %s", expected, actual)
+	}
+}
+
+func Test_CheckRemandsDateProvincial_Input_10imprisonDate_ShouldBe_True(t *testing.T) {
+	input := 10
+	expected := true
+	actual := imprison_date(input)
+
+	if expected != actual {
+		t.Errorf("expected %s but it got %s", expected, actual)
+	}
+}
+
+func Test_CheckRemandsDateProvincial_Input_15imprisonDate_ShouldBe_True(t *testing.T) {
+	input := 15
+	expected := false
+	actual := imprison_date(input)
+
+	if expected != actual {
+		t.Errorf("expected %s but it got %s", expected, actual)
+	}
+}
