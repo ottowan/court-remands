b0c02866244b4fc2b6ec1191133fb9a07e06e93d
thecob <14883363+thecob@users.noreply.github.com>
2019-10-31 21:05:19 +0700

    Add files via upload

--- paenaltyrate_test.go
+++ paenaltyrate_test.go
@@ -1,1 +1,30 @@
 package main
+
+import "testing"
+func Test_CheckFineRateKwaeng_Input_punishyear_1month_ShouldBe_True(t *testing.T) {
+	input := -1
+	expected := false
+	actual := Punish(input)
+
+	if expected != actual {
+		t.Errorf("expected %s but it got %s", expected, actual)
+	}
+}
+func Test_CheckFineRateKwaeng_Input_punishyear_3year1month_ShouldBe_True(t *testing.T) {
+	input := 3.1
+	expected := false
+	actual := Punish(input)
+
+	if expected != actual {
+		t.Errorf("expected %s but it got %s", expected, actual)
+	}
+}
+func Test_CheckFineRateKwaeng_Input_punishyear_1year_ShouldBe_True(t *testing.T) {
+	input := 1
+	expected := True
+	actual := Punish(input)
+
+	if expected != actual {
+		t.Errorf("expected %s but it got %s", expected, actual)
+	}
+}
