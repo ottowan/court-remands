071158f61b0fe634f1778f491663ec860418c050
thecob <14883363+thecob@users.noreply.github.com>
2019-11-01 00:28:10 +0700

    remandsdate_test.go

--- paenaltyrate_test.go
+++ paenaltyrate_test.go
@@ -1,1 +1,31 @@
 package main
+
+import "testing"
+
+func Test_CheckPenalityRateKwaeng_Input_punishyear_1month_ShouldBe_True(t *testing.T) {
+	input := -1
+	expected := false
+	actual := CheckPenalityRateKwaeng(input)
+
+	if expected != actual {
+		t.Errorf("T4 :expected %s but it got %s", expected, actual)
+	}
+}
+func Test_CheckPenalityRateKwaeng_Input_punishyear_3year1month_ShouldBe_True(t *testing.T) {
+	input := 37
+	expected := false
+	actual := CheckPenalityRateKwaeng(input)
+
+	if expected != actual {
+		t.Errorf("T5 : expected %s but it got %s", expected, actual)
+	}
+}
+func Test_CheckPenalityRateKwaeng_Input_punishyear_1year_ShouldBe_True(t *testing.T) {
+	input := 12
+	expected := true
+	actual := CheckPenalityRateKwaeng(input)
+
+	if expected != actual {
+		t.Errorf("T6 :expected %s but it got %s", expected, actual)
+	}
+}
--- remandsdate_test.go
+++ remandsdate_test.go
@@ -1,1 +1,42 @@
 package main
+import "testing"
+
+func Test_CheckRemandsDateProvincial_Input_Max10year_0imprisonDate_ShouldBe_True(t *testing.T) {
+	input := 0
+	expected := false
+	actual := CheckRemandsDateProvincial(input)
+
+	if expected != actual {
+		t.Errorf("T44 :expected %s but it got %s", expected, actual)
+	}
+}
+
+func Test_CheckRemandsDateProvincial_Input_Max10year_1imprisonDate_ShouldBe_True(t *testing.T) {
+	input := 1
+	expected := true
+	actual := CheckRemandsDateProvincial(input)
+
+	if expected != actual {
+		t.Errorf("T45 :expected %s but it got %s", expected, actual)
+	}
+}
+
+func Test_CheckRemandsDateProvincial_Input_Max10year_10imprisonDate_ShouldBe_True(t *testing.T) {
+	input := 10
+	expected := true
+	actual := CheckRemandsDateProvincial(input)
+
+	if expected != actual {
+		t.Errorf("T46 :expected %s but it got %s", expected, actual)
+	}
+}
+
+func Test_CheckRemandsDateProvincial_Input_Max10year_15imprisonDate_ShouldBe_True(t *testing.T) {
+	input := 15
+	expected := false
+	actual := CheckRemandsDateProvincial(input)
+
+	if expected != actual {
+		t.Errorf("T47 :expected %s but it got %s", expected, actual)
+	}
+}
