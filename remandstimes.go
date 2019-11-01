85fc95501e1c69eb06fe191e1e5355bf1ce1d4ba
parinya.coj <parinya.coj@gmail.com>
2019-11-01 18:28:10 +0700

    [Create] implement remands times & date to test all PASS

--- remandstimes.go
+++ remandstimes.go
@@ -1,17 +1,29 @@
 package main
 
 func CheckRemandsTimesKwaeng(input int) bool {
+	if input >= 1 && input <= 5 {
+		return true
+	}
 	return false
 }
 
 func CheckRemandsTimesLessRatesProvincial(input int) bool {
+	if input == 1 {
+		return true
+	}
 	return false
 }
 
 func CheckRemandsTimesMiddleRateProvincial(input int) bool {
+	if input >= 1 && input <= 4 {
+		return true
+	}
 	return false
 }
 
 func CheckRemandsTimesTopRateProvincial(input int) bool {
+	if input >= 1 && input <= 7 {
+		return true
+	}
 	return false
 }
