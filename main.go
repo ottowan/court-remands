cbd5d5109998899f2bff26fd0806052e7c5937ca
parinya.coj <parinya.coj@gmail.com>
2019-11-01 13:32:05 +0700

    [Create] make new  testcase to CALL API /v1/ping

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
+	GetMainEngine().Run(":8080")
+}
+
+func GetMainEngine() *gin.Engine {
 	r := gin.Default()
-	r.GET("/ping", ping)
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/ping", ping)
+	}
+	return r
 }
 
 func ping(c *gin.Context) {
---
+++ main_test.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+// func Test_ping(t *testing.T) {
+// 	t.Run("Call API /ping ShouldBe 200ok", func(t *testing.T) {
+// 		request, _ := http.NewRequest(http.MethodGet, "/ping", nil)
+// 		response := httptest.NewRecorder()
+
+// 		got := response.Body.String()
+// 		want := "20"
+
+// 		if got != want {
+// 			t.Errorf("got %q, want %q", got, want)
+// 		}
+// 	})
+// }
+
+func performRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest(method, path, nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
+
+func Test_ping(t *testing.T) {
+	// Build our expected body
+	body := gin.H{
+		"message": "pong",
+	}
+
+	// Grab our router
+	router := GetMainEngine()
+	// Perform a GET request with that handler.
+	w := performRequest(router, "GET", "/v1/ping")
+
+	// Test StatusOK(200) from response
+	t.Run("GET HTTP StatusOK", func(test *testing.T) {
+		expected := http.StatusOK
+		actual := w.Code
+		if expected != actual {
+			t.Errorf(" expected %v but it got %v", expected, actual)
+		}
+	})
+
+	// Test Convert the JSON response to a map
+	var response map[string]string
+	err := json.Unmarshal([]byte(w.Body.String()), &response)
+	t.Run("Convert Body to Unmarhal", func(test *testing.T) {
+		if err != nil {
+			t.Errorf(" expected %v but it got %v", "not nil", err)
+		}
+	})
+
+	t.Run("Expected body['message'] equal response['message']", func(test *testing.T) {
+
+		expected := body["message"]
+		actual := response["message"]
+		if expected != actual {
+			t.Errorf(" expected %v but it got %v", expected, actual)
+		}
+	})
+}
